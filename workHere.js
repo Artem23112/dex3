8c3cd30ba8e4f98886eb57381e685c97f81f0c63
Arem23112 <yartemtchernichuk@yandex.ru>
2022-02-12 18:48:59 +0200

    write all functions

--- workHere.js
+++ workHere.js
@@ -2,7 +2,8 @@ export const processCartData = (cartData) => {
   //TODO: Нужно добавить поле discount(oldPrice - price)
   // убрать поле oldPrice
   return cartData.map((obj) => {
-    if (!(obj.hasOwnProperty("oldPrice") && obj.hasOwnProperty('price'))) return obj;
+    if (!(obj.hasOwnProperty("oldPrice") && obj.hasOwnProperty("price")))
+      return obj;
 
     obj.discount = obj.oldPrice - obj.price;
     delete obj.oldPrice;
@@ -25,40 +26,75 @@ export const calcSum = (cartData) => {
     total: { count: 0, sum: 0 },
     water: { count: 0, sum: 0 },
     pizza: { count: 0, sum: 0 },
-    other: { count: 0, sum: 0 },
+    other: { count: 0, sum: 0 }
   };
 
-  cartData.forEach(obj => {
+  cartData.forEach((obj) => {
     res[obj.type].count += obj.count;
     res[obj.type].sum += obj.price * obj.count;
-    res['total'].count += obj.count;
-    res['total'].sum += obj.price * obj.count;
+    res["total"].count += obj.count;
+    res["total"].sum += obj.price * obj.count;
   });
-  
+
   return res;
 };
 
 export const getCartItemsByDate = (cartData, date) => {
   //TODO: выбрать покупки сделанные за выбранную дату
-  const selectedData = new Date(date);
-  const DATE = new Date(selectedData.getFullYear(), selectedData.getMonth(), selectedData.getDate());
-  return cartData.filter(obj => {
-    return new Date(obj.date).getTime() === DATE.getTime();
-  })
+  const selectedDate = new Date(date);
+  const dateToCompare = new Date(
+    selectedDate.getFullYear(),
+    selectedDate.getMonth(),
+    selectedDate.getDate()
+  );
 
+  return cartData.filter((obj) => {
+    const tempDate = new Date(obj.date);
+    const orderDate = new Date(
+      tempDate.getFullYear(),
+      tempDate.getMonth(),
+      tempDate.getDate()
+    );
+    return orderDate.getTime() === dateToCompare.getTime();
+  });
 };
 
 export const repeatOrder = (cartData, date) => {
-  //TODO: нужно повторить заказ за выбранную дату
-  // дублиовать соответствующие элементы
-  // поставить в начало спиcка
-  // дату текущую
-  // поменять id на уникальный
+  const nowDate = new Date();
+  const selectedDate = new Date(date);
+  // создаю объект Date без учета времени, с учетом только даты, потому что сравнение должно быть по дате
+  const dateToCompare = new Date(
+    selectedDate.getFullYear(),
+    selectedDate.getMonth(),
+    selectedDate.getDate()
+  ); // объект для сравнения
+  const arrOfFoundObjs = []; //временное хранилище всех найденных заказов
+
+  cartData.forEach((obj) => {
+    const tempDate = new Date(obj.date); // временнЫй объект Date, используется для того чтобы создать другой объект без учета времени
+    // создаю объект Date без учета времени, с учетом только даты, потому что сравнение должно быть по дате
+    const orderDate = new Date(
+      tempDate.getFullYear(),
+      tempDate.getMonth(),
+      tempDate.getDate()
+    );
+
+    // поиск соответсвующих заказов
+    if (dateToCompare.getTime() === orderDate.getTime()) {
+      const copyObj = JSON.parse(JSON.stringify(obj)); // копирование объекта
+
+      copyObj.date = nowDate.toISOString(); // присваиваю сегоднешнюю дату в формате строки
+      copyObj.id = nowDate.getTime(); // устанавливаю уникальный id путем присвоения количества миллисекунд прошедших с 1970
+      arrOfFoundObjs.push(copyObj);
+    }
+  });
+
+  cartData = arrOfFoundObjs.concat(cartData);
   return cartData;
 };
 
 export const addItem = (cartData, item) => {
-  //TODO: увеличить кол-во товара в полученном элементе
+  item.count += 1;
   return cartData;
 };
 
@@ -67,9 +103,15 @@ export const checkPromo = (cartData) => {
   // проверить что суммарно в корзине больше 1000р
   // что есть пункт больше чем на 500р
   // что нет скидочных товаров
-  return {
-    total: false,
+  const tryPromo = {
+    total: Boolean(calcSum(cartData).total.sum > 1000),
     oneBigPosition: false,
-    notDiscounted: false
+    notDiscounted: true
   };
+  for (const item of cartData) {
+    if (item.count * item.price > 500) tryPromo.oneBigPosition = true;
+    if (item.hasOwnProperty("discount")) tryPromo.notDiscounted = false;
+  }
+
+  return tryPromo;
 };
